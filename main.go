a9ac8b9b3e71995315ec5c59b316257901d328d9
ibrahimfe <ibrahiimm.f@gmail.com>
2023-11-20 07:36:28 +0700

    add 2 pages

--- main.go
+++ main.go
@@ -5,15 +5,33 @@ import (
 	"net/http"
 )
 
+// HTTP Handler
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello From SnippetBox"))
+
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display Spesific Snippet ...."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new Snippet...."))
 }
 
 func main() {
+	// Controller for HTTP Request
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Println("Starting servern on :4000")
+	// Start http server on port 4000
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
